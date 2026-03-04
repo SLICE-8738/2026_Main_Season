109178a39e3fe7145bacc3350e495e63418b77ed
KittenMeow <crazypurrfect@gmail.com>
2026-03-04 13:03:06 -0500

    Change right shooter to velocity

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -68,7 +68,7 @@ public class Shooter extends SubsystemBase {
     VelocityVoltage request = new VelocityVoltage(0).withSlot(0);
 
     leftShooterMotor.setControl(request.withVelocity(speed));
-    rightShooterMotor.setControl(request.withAcceleration(speed));
+    rightShooterMotor.setControl(request.withVelocity(speed));
 
   }
 
